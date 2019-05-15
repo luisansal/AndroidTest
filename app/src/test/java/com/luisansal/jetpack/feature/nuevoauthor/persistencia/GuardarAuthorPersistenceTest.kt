431443cb60f79c529490bdb6f0a857bab2585a16
luisansal <luisansaldev@gmail.com>
2019-05-15 17:40:11 -0500

    STEP1

--- app/src/test/java/com/luisansal/jetpack/feature/nuevoauthor/persistencia/GuardarAuthorPersistenceTest.kt
+++ app/src/test/java/com/luisansal/jetpack/feature/nuevoauthor/persistencia/GuardarAuthorPersistenceTest.kt
@@ -1,30 +1,48 @@
 package com.luisansal.jetpack.feature.nuevoauthor.persistencia
 
 import com.luisansal.jetpack.dagger.base.BaseIntegrationTest
+import com.luisansal.jetpack.model.domain.Author
+import com.luisansal.jetpack.model.repository.interfaces.AuthorRepository
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import javax.inject.Inject
 
 class GuardarAuthorPersistenceTest : BaseIntegrationTest() {
 
+    @Inject
+    lateinit var authorRepository: AuthorRepository
+
     @Before
     fun setup() {
-
+        daggerComponent.inject(this)
     }
 
     @After
     fun after() {
-
+        authorRepository.db?.close()
     }
 
     @Test
     fun `guardar author`() {
+        val author = Author("0000001","Luis","Sanchez")
+        val completableTest = authorRepository.guardarAuthor(author).test()
+        completableTest.awaitTerminalEvent()
 
+        completableTest.assertNoErrors().assertComplete()
     }
 
     @Test
     fun encontrarAuthor() {
+        `guardar author`()
 
+        val dni = "0000001"
+        val singleTest = authorRepository.buscarAuthorByDni(dni).test()
+        singleTest.awaitTerminalEvent()
+
+        singleTest.assertNoErrors().assertValue {
+            it.dni.equals(dni)
+        }
     }
 
 }
